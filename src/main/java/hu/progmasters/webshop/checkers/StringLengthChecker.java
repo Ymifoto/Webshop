3ce643cb347fea3c3cd4a84bff6ec90945eaf03f
Ymifoto <48827837+Ymifoto@users.noreply.github.com>
2022-04-08 09:27:28 +0200

    StringLengthChecker - add message

--- src/main/java/hu/progmasters/webshop/checkers/StringLengthChecker.java
+++ src/main/java/hu/progmasters/webshop/checkers/StringLengthChecker.java
@@ -1,9 +1,16 @@
 package hu.progmasters.webshop.checkers;
 
+import hu.progmasters.webshop.handlers.OutputHandler;
+
 public class StringLengthChecker implements Checker {
 
     @Override
     public boolean check(String input) {
-        return input.length() > 0;
+
+        if (input.length() <= 0) {
+            OutputHandler.outputRed("Required field");
+            return false;
+        }
+        return true;
     }
 }
